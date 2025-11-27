a0b29a2a0381f7ac19bd1887aa9dfabd636aab4f
mariemazzouz581-droid <mariemazzouz581@gmail.com>
2025-11-27 14:58:25 +0100

    tp tableau

---
+++ tableau/tp 1.java
@@ -0,0 +1,12 @@
+public class tp1 {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> notes= new ArrayList<>();
+ 
+        do{
+            system.out.println("saisir n notes");
+            n=sc.nextInt();
+        }while ( n>0); 
+    
+        
+    }
+}
